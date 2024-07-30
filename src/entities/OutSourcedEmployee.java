47b527da85fe45041de637dfcd95a89a3e2b57b8
Miguel <migueldoiche2@gmail.com>
2024-07-29 23:52:37 -0300

    Created project and classes

---
+++ src/entities/OutSourcedEmployee.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class OutSourcedEmployee extends Employee {
+    private Double additionalCharge;
+
+    public OutSourcedEmployee(String name, Integer hour, Double valuePerHour, Double additionalCharge) {
+        super(name, hour, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+    @Override
+    public Double payment(){
+        return super.payment() + additionalCharge * 1.1;
+    }
+}
